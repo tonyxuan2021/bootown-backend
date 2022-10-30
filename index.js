3971babc791be04d4e6f990174dde3eeafe1c38a
tonyxuan2021 <tonyyexuan@gmail.com>
2022-10-29 20:35:51 -0700

    add frontend

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const userRoutes = require("./routes/users");
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+app.use(express.static(path.join(__dirname, 'build')))
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
 
 app.use("/api/users", userRoutes);
 
